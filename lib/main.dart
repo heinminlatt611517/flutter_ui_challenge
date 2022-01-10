9861dd2f23e27dd33c73d307f84e0b51e2e2ebfb
Sai Hein Min Latt <heinminlatt.dev1@gmail.com>
2022-01-11 00:41:25 +0630

    init

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ui_challenge/detail_page.dart';
 
 void main() {
+  //share on git
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
